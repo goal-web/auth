c9f65dda7694732de3a9b80a5f28c29594a9a3c9
qbhy <96qbhy@gmail.com>
2022-03-29 22:04:47 +0800

    feat: 优化中间件默认参数

--- middleware.go
+++ middleware.go
@@ -6,7 +6,11 @@ import (
 )
 
 func Guard(guards ...string) interface{} {
-	return func(request contracts.HttpRequest, next contracts.Pipe, auth contracts.Auth) interface{} {
+	return func(request contracts.HttpRequest, next contracts.Pipe, auth contracts.Auth, config contracts.Config) interface{} {
+
+		if len(guards) == 0 {
+			guards = append(guards, config.Get("auth").(Config).Defaults.Guard)
+		}
 
 		for _, guard := range guards {
 			if auth.Guard(guard, request).Guest() {
